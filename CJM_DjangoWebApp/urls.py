3bdcdc3085383d2939876f8b7331b635e6de731a
CarlDev16 <YOUR_GITHUB_EMAIL>
2026-09-23 23:40:11 +0800

    Implement AJAX Student Dashboard

---
+++ CJM_DjangoWebApp/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+
+    path("", include("home.urls")),
+
+    path("registration/", include("registration.urls")),
+
+    path(
+        "api/",
+        include("registration.api_urls")
+    ),
+     
+    path(
+        "accounts/",
+        include("django.contrib.auth.urls")
+    ),
+    
+]  
